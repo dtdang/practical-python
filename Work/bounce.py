4a300f0af8e9347767afc5d582d2cb7283e767aa
Dalena <mimidalena@gmail.com>
2021-04-09 11:39:34 -0700

    section 1 complete

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+i = 1
+while i < 11:
+	height *= 3/5
+	print(f"{i} {round(height, 4)}")
+	i += 1
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,44 @@
 # pcost.py
 #
 # Exercise 1.27
+# def portfolio_cost(filename):
+# 	total_cost = 0
+# 	with open(filename, 'rt') as f:
+# 		headers = next(f).split(',')
+# 		for line in f:
+# 			row = line.split(',')
+# 			if not row[1]:
+# 				print(row[0] + " No Shares")
+# 				continue
+# 			cost = int(row[1]) * float(row[2])
+# 			total_cost += cost
+# 	return total_cost
+
+
+# cost = portfolio_cost('Data/portfolio.csv')
+# print('Total Cost', cost)
+
+#1.33
+import sys
+import csv
+
+def portfolio_cost(filename):
+	total_cost = 0
+	f = open(filename)
+	rows= csv.reader(f)
+	headers= next(rows)
+	for row in rows:
+		if not row[1]:
+			print(row[0] + " No Shares")
+			continue
+		cost = int(row[1]) * float(row[2])
+		total_cost += cost
+	return total_cost
+
+if len(sys.argv) == 2:
+	filename = sys.argv[1]
+else:
+	filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total Cost: ', cost)
